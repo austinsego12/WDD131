02e7089ceb987778e46590b85bd7c80a1ed3ef19
austinsego12 <dragoastee123@gmail.com>
2026-01-18 10:42:15 -0600

    added byui dark mode image, and changed css file name from mission to styles to match index link

--- mission/mission.js
+++ mission/mission.js
@@ -6,11 +6,12 @@ function changeTheme() {
 
   if (selectedTheme === "dark") {
     document.body.classList.add("dark");
-    logo.setAttribute("src", "images/byui-logo-white.webp");
+    logo.setAttribute("src", "images/byui-logo-white.png");
   } else {
     document.body.classList.remove("dark");
-    logo.setAttribute("src", "images/byui-logo.webp");
+    logo.setAttribute("src", "images/byui-logo.png");
   }
 }
 
 themeSelector.addEventListener("change", changeTheme);
+
