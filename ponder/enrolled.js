27583d3475cbd76defac0b6acebf0eb7456b2a8b
austinsego12 <dragoastee123@gmail.com>
2026-02-25 11:42:30 -0600

    completed enrolled students assignment

---
+++ ponder/enrolled.js
@@ -0,0 +1,51 @@
+const aCourse = {
+  code: "CSE121b",
+  name: "Javascript Language",
+  logo: "images/js-logo.png",
+  sections: [
+    { sectionNum: 1, roomNum: "STC 353", enrolled: 26, days: "TTh", instructor: "Bro T" },
+    { sectionNum: 2, roomNum: "STC 347", enrolled: 28, days: "TTh", instructor: "Sis A" }
+  ],
+  enrollStudent: function (sectionNum) {
+    const num = Number(sectionNum);
+
+    const sectionIndex = this.sections.findIndex(
+      (section) => section.sectionNum === num
+    );
+
+    if (sectionIndex >= 0) {
+      this.sections[sectionIndex].enrolled++;
+      renderSections(this.sections);
+    } else {
+      console.log(`Section ${num} not found`);
+    }
+  }
+};
+
+function sectionTemplate(section) {
+  return `<tr>
+    <td>${section.sectionNum}</td>
+    <td>${section.roomNum}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.days}</td>
+    <td>${section.instructor}</td>
+  </tr>`;
+}
+
+function renderSections(sections) {
+  document.querySelector("#sections").innerHTML =
+    sections.map(sectionTemplate).join("");
+}
+
+// headings
+document.querySelector("#courseName").textContent = aCourse.name;
+document.querySelector("#courseCode").textContent = aCourse.code;
+
+// initial render
+renderSections(aCourse.sections);
+
+// click event
+document.querySelector("#enrollStudent").addEventListener("click", () => {
+  const sectionNum = document.querySelector("#sectionNumber").value;
+  aCourse.enrollStudent(sectionNum);
+});
